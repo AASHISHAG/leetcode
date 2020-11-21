c2d9ef2fdbcff212ac25f4b114eeaf4ee3eae3ef
AASHISHAG <agarwalaashish20@gmail.com>
2020-11-21 19:24:41 +0100

    Larget alphabetic character.

---
+++ string/larget-alphabetic-character.py
@@ -0,0 +1,30 @@
+# https://www.geeksforgeeks.org/find-the-largest-alphabetic-character-present-in-the-string/
+# microsoft codility
+"""
+Given a string str, our task is to find the Largest Alphabetic Character, whose both uppercase and lowercase are present in the string.
+The uppercase character should be returned. If there is no such character then return -1 otherwise print the uppercase letter of the character.
+"""
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+def largest_alphabetic_charcater(text: str) -> chr:
+    text = list(text)
+    lower = [False]*27
+    upper = [False]*27
+
+    a = ord('a')
+    A = ord('A')
+    for index, ch in enumerate(text):
+        if ch.islower(): lower[ord(ch)-a] = True 
+        else: upper[ord(ch)-A]  = True
+
+    for index in range(26,-1,-1):
+        if lower[index] and upper[index]:
+            return chr(index + ord("A"))
+    return -1
+
+if __name__ == '__main__':
+    text = "admeDCAB"
+    #text = "dAeB"
+    ch = largest_alphabetic_charcater(text)
+    print(ch)
