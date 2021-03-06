165af338641e4f315568a336060f2361d1e726f7
AASHISHAG <agarwalaashish20@gmail.com>
2021-03-06 13:30:01 +0100

    Added Meeting Rooms

--- integers/meeting-rooms.py
+++ integers/meeting-rooms.py
@@ -13,6 +13,22 @@ Input: [[7,10],[2,4]]
 Output: True
 """
 
-# Time Complexity: O()
-# Space Complexity: O()
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
 def find_possibility(input):
+    slot = [0]*59
+
+    for start, end in input:
+        if sum(slot[start:end+1]) > 0:
+            return False
+        else:
+            slot[start:end+1] = [1]*(end+1-start)
+    return True       
+
+if __name__ == '__main__':
+    input1 = [[0,30],[5,10],[15,20]]
+    input2 = [[7,10],[2,4]]
+
+    result = find_possibility(input2)
+    print(result)
