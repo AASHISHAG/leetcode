af1e0514d56b811145f7a05bfdfe269d9387c1a3
Aashish Agarwal <45683843+AASHISHAG@users.noreply.github.com>
2023-01-08 19:41:31 +0100

    Update tree-traversal.py

--- binary-tree/tree-traversal.py
+++ binary-tree/tree-traversal.py
@@ -12,6 +12,47 @@ def print_tree(node):
     # print(node.data) # - inorder (4,2,5,1,6,3,7)
     if node.right: print_tree(node.right)
     print(node.data) # - postorder (4,5,2,6,7,3,1)
+    
+    '''
+    1. Post order traversal
+    '''
+    '''
+    if not root: return
+    if root.left: self.invertTree(root.left)
+    if root.right: self.invertTree(root.right)
+    root.left, root.right = root.right, root.left
+    return root
+    '''
+
+    '''
+    2. Level order traversal - DFS
+    '''
+
+    '''
+    if root is None: return
+    stack = [root]
+    while stack:
+        node = stack.pop()
+        if node.left: stack.append(node.left)
+        if node.right: stack.append(node.right)
+        node.left, node.right = node.right, node.left
+    return root
+    '''
+
+    '''
+    3. Level order traversal - BFS
+    '''
+        
+    '''
+    if root is None: return
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        if node.left: queue.append(node.left)
+        if node.right: queue.append(node.right)
+        node.left, node.right = node.right, node.left
+    return root
+    '''
 
 
 if __name__ == '__main__':
